ac0553489bf576446a8f3da04feab0e4eb16429e
Michael <micoos@gmail.com>
2023-08-28 15:38:14 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useRef } from 'react';
 
 function App() {
+  const [response, setResponse] = useState('');
+  const messageRef = useRef(null);
+
+  const handleRestartDisplay = () => {
+    fetch('http://127.0.0.1:8090/restart')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to restart display');
+        }
+        setResponse('Display restarted successfully');
+        setTimeout(() => {
+          setResponse('');
+        }, 10000);
+      })
+      .catch(error => {
+        console.error(error);
+        setResponse('Failed to restart display');
+        setTimeout(() => {
+          setResponse('');
+        }, 10000);
+      });
+  };
+
+  const handleShowMessage = message => {
+    if(message==='') { 
+      setResponse('Please enter a message');
+      setTimeout(() => {
+        setResponse('');
+      }, 10000);
+      return;
+    }
+    fetch(`http://127.0.0.1:8090/show?message=${message}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to show message');
+        }
+        messageRef.current.value = '';
+        setResponse(`Message "${message}" shown successfully`);
+        setTimeout(() => {
+          setResponse('');
+        }, 10000);
+      })
+      .catch(error => {
+        console.error(error);
+        setResponse('Failed to show message');
+        setTimeout(() => {
+          setResponse('');
+        }, 10000);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleRestartDisplay}>Restart Display</button>
+      <button onClick={() => handleShowMessage(messageRef.current.value)}>Show Message</button>
+      <textarea type="text" id="message" ref={messageRef} />
+      {response && <div className="status">{response}</div>}
     </div>
   );
 }
