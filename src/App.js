2728f0a8d7ca3a88ae9add2bc701031de704ea39
Michael Oostindiën <michael.o@avantage.nl>
2023-08-29 19:37:46 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import React, { useState, useRef } from 'react';
 import './App.css';
+import mqtt from 'mqtt/dist/mqtt'
+
+const mqttClient  = mqtt.connect('ws://raspberrypi:9001', { clientId: 'control_' + Math.random().toString(16).substr(2, 8)})
+
+mqttClient.on('connect', function () {
+  mqttClient.subscribe('control/action', function (err) { console.log(err) }); //to do error handling
+});
+
+mqttClient.on("message", (topic, message) => {
+  // message is Buffer
+  console.log(message.toString());
+});
+
+const reconnect = () => {
+  setTimeout(() => {
+    mqttClient.reconnect();
+  }, 5000);
+}
+
+mqttClient.on('close', () => {
+  console.log('Connection closed. Trying to reconnect...');
+  reconnect();
+});
+
 
 function App() {
   const [response, setResponse] = useState('');
@@ -7,47 +31,31 @@ function App() {
   const urlRef = useRef(null);
 
   const handleRestartDisplay = () => {
-    fetch('http://raspberrypi:8090/restart', { method: 'GET' })
-      .then(response => {
-        console.log(response);
-        setResponse('Display restarted successfully');
-        setTimeout(() => {
-          setResponse('');
-        }, 10000);
-      })
-      .catch(error => {
-        console.error(error);
-        setResponse('Failed to restart display');
-        setTimeout(() => {
-          setResponse('');
-        }, 10000);
-      });
+    mqttClient.publish('control/action', 'restart')
+    setResponse('Display restarted successfully');
+    setTimeout(() => {
+      setResponse('');
+    }, 10000);
+  };
+
+  const handleShowGallery = () => {
+    mqttClient.publish('control/action', 'gallery')
+    setResponse('Display changed successfully to gallery');
+    setTimeout(() => {
+      setResponse('');
+    }, 10000);
   };
 
   const handleShowMessage = (message) => {
-    fetch('http://raspberrypi:8090/message', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ message })
-    })
-      .then(response => {
-        console.log(response);
-        if (response.status !== 200) {
-          throw new Error('Failed to show message');
-        }
-        messageRef.current.value = '';
-        setResponse(`Message "${message}" shown successfully`);
-        setTimeout(() => {
-          setResponse('');
-        }, 10000);
-      })
-      .catch(error => {
-        console.error(error);
-        setResponse('Failed to show message');
-        setTimeout(() => {
-          setResponse('');
-        }, 10000);
-      });
+    //strip : from message
+    message = message.replace(/:/g, '');
+    mqttClient.publish('control/action', 'message:' + message)
+    setResponse(`Message "${message}" shown successfully`);
+    setTimeout(() => {
+      setResponse('');
+    }, 10000);
+
+    messageRef.current.value = '';
   };
 
   const handleScrapeUrl = () => {
@@ -85,8 +93,13 @@ function App() {
       <button onClick={handleRestartDisplay}>Restart Display</button>
       <input placeholder="https//eenurlhier.nl" type="text" id="url" ref={urlRef} />
       <button onClick={handleScrapeUrl}>Scrape URL</button>
+      <button onClick={handleShowGallery}>show gallery</button>
       <button onClick={() => handleShowMessage(messageRef.current.value)}>Show Message</button>
-      <textarea placeholder="je bericht" type="text" id="message" ref={messageRef} />      
+      <textarea placeholder="je bericht" type="text" id="message" ref={messageRef} onKeyDown={(event) => {
+        if (event.keyCode === 13) {
+          handleShowMessage(messageRef.current.value);
+        }
+      }} />
       {response && <div className="status">{response}</div>}
     </div>
   );
